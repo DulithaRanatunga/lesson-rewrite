526d9cc10ddd3da9448fb5f09bc2869c651edfcb
Dulitha Ranatunga <dulitha@canva.com>
2023-07-20 11:56:02 +1000

    App can read page content and replace it

--- src/app.tsx
+++ src/app.tsx
@@ -2,25 +2,63 @@ import { Button, Rows, Text } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
 import * as React from "react";
 import styles from "styles/components.css";
+import { SelectionEvent, selection } from "@canva/preview/design";
+
 
 export const App = () => {
-  const onClick = () => {
-    addNativeElement({
-      type: "TEXT",
-      children: ["Hello world!"],
+  const [event, setEvent] = React.useState<SelectionEvent<"text"> | undefined>();
+
+  React.useEffect(() => {
+    selection.registerOnChange({
+      scope: "text",
+      onChange: (event) => {
+        setEvent(event);
+      },
     });
-  };
+  }, []);
+
+  const isElementSelected = event && event.count > 0;
+
+  async function handleReplace() {
+    if (!event || !isElementSelected) {
+      return;
+    }
+
+    await selection.setContent(event, () => {
+      return {
+        text: "You updated the selected text!",
+      };
+    });
+  }
+
+  async function handleAdd() {
+    if (!event || !isElementSelected) {
+      return;
+    }
+
+    await selection.setContent(event, async (value) => {
+      console.log(value);
+      await addNativeElement({
+        type: "TEXT",
+        children: [value.text],
+        color: "#ff0099",
+        fontWeight: "bold"
+      });
+      return value;
+    });
+  }
 
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
         <Text>
-          To make changes to this app, edit the{" "}
-          <code className={styles.code}>src/app.tsx</code> file, then close and
-          reopen the app in the editor to preview the changes.
+          Select some content, then hit the button.
         </Text>
-        <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
+        <Button variant="primary" onClick={handleReplace} disabled={!isElementSelected} stretch>
+          Replace
+        </Button>
+        <Button variant="primary" onClick={handleAdd} disabled={!isElementSelected} stretch>
+          Add new
         </Button>
       </Rows>
     </div>
