4851b39a3f7f5ee379c2f35da215be8a91ec1466
Dulitha Ranatunga <dulitha@canva.com>
2023-07-20 13:11:26 +1000

    Communicate with BE server, update UI based on BE response

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,15 @@ import { Button, Rows, Text } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
 import * as React from "react";
 import styles from "styles/components.css";
+import { auth } from "@canva/user";
 import { SelectionEvent, selection } from "@canva/preview/design";
 
+const BACKEND_URL = `${BACKEND_HOST}/transform`;
+
+type State = "idle" | "loading" | "success" | "error";
 
 export const App = () => {
+  const [state, setState] = React.useState<State>("idle");
   const [event, setEvent] = React.useState<SelectionEvent<"text"> | undefined>();
 
   React.useEffect(() => {
@@ -19,32 +24,62 @@ export const App = () => {
 
   const isElementSelected = event && event.count > 0;
 
-  async function handleReplace() {
-    if (!event || !isElementSelected) {
-      return;
-    }
+  async function callTransformApi(text) {
+    try {
+      setState("loading");
+      const token = await auth.getCanvaUserToken();
+      const res = await fetch(BACKEND_URL, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text
+        })
+      });
 
-    await selection.setContent(event, () => {
-      return {
-        text: "You updated the selected text!",
-      };
+      const body = await res.json();
+      setState("success");
+      return body;
+    } catch (error) {
+      setState("error");
+      console.error(error);
+    }
+  };
+
+
+  async function handleReplace() {
+    executeOnEachSelectedElement(async (value) => {
+      // Send the text to the backend
+      // Wait for response
+      // Transform to response
+      const response = await callTransformApi(value.text);
+      return { text: response.text };
     });
   }
 
   async function handleAdd() {
+    executeOnEachSelectedElement(async (value) => {
+      const response = await callTransformApi(value.text);
+      await addNativeElement({
+        type: "TEXT",
+        children: [response.text],
+        color: "#ff0099",
+        fontWeight: "bold"
+      });
+      return value;
+    })
+  }
+
+  async function executeOnEachSelectedElement(functionToExecute) {
     if (!event || !isElementSelected) {
       return;
     }
 
     await selection.setContent(event, async (value) => {
-      console.log(value);
-      await addNativeElement({
-        type: "TEXT",
-        children: [value.text],
-        color: "#ff0099",
-        fontWeight: "bold"
-      });
-      return value;
+      // Ignore selections which don't have text.
+      return !!value.text ? functionToExecute(value): value;
     });
   }
 
@@ -54,12 +89,18 @@ export const App = () => {
         <Text>
           Select some content, then hit the button.
         </Text>
-        <Button variant="primary" onClick={handleReplace} disabled={!isElementSelected} stretch>
+        <Button variant="primary" onClick={handleReplace} disabled={!isElementSelected} loading={state === "loading"} stretch>
           Replace
         </Button>
-        <Button variant="primary" onClick={handleAdd} disabled={!isElementSelected} stretch>
+        <Button variant="primary" onClick={handleAdd} disabled={!isElementSelected} loading={state === "loading"} stretch>
           Add new
         </Button>
+        {state === "success" && (
+          <Text>Success!</Text>
+        )}
+        {state === "error" && (        
+          <Text>Something went wrong</Text>
+        )}
       </Rows>
     </div>
   );
